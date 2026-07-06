34e5c6abc30f52e35a57aa3dce0028b33e7dd367
JoseAngelDev2 <casuhud1@gmail.com>
2026-07-06 17:49:25 -0400

    Add files via upload

---
+++ SalonFlow/src/Models/Client.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalonFlow.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public int Active { get; set; } = 1;
+    }
+    }

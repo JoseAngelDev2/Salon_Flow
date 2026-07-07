d87ffa292847f09ac02e34efd1c8221e32fa525c
JoseAngelDev2 <casuhud1@gmail.com>
2026-07-06 22:01:57 -0400

    Integrate FluentValidation and Swagger in Program.cs
    Added FluentValidation and Swagger support to the application.

--- SalonFlow/src/Program.cs
+++ SalonFlow/src/Program.cs
@@ -1,9 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using src.Data;
+using FluentValidation;
+using WebApplication1.FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+
+builder.Services.AddValidatorsFromAssemblyContaining<CustomerCreateDtoValidator>();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var MyConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddCors(options =>
 {
@@ -15,20 +24,26 @@ builder.Services.AddCors(options =>
     });
 });
 
-var MyConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<CustomerDbContext>(x =>
 {
     x.UseNpgsql(MyConnectionString);
 });
 
+
 var app = builder.Build();
 
-
-app.UseHttpsRedirection();
-app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+};
 
 app.UseCors("AllowAll");  
 app.UseAuthorization();
+
+app.MapControllers();
+
+
+//app.UseAuthorization();
 
 app.Run();
